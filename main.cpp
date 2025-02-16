2e010e3d750172ec2e3715cfa8ead06691be14fb
gaminghusky1 <Justin_Ma@outlook.com>
2025-02-15 17:07:50 -0800

    Made FPS consistent and added a sprite (copied from ghost game); however, the code quality is not the best right now so will need to fix in the future. Also need to find a new sprite.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <iostream>
 
-const int SCREEN_WIDTH = 500;
-const int SCREEN_HEIGHT = 500;
-const int CIRCLE_RADIUS = 5;
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 1000;
+const int PLAYER_HEIGHT = 100;
+const int PLAYER_WIDTH = 100;
+const int FPS = 60;
+const double frameTime = 1.0 / FPS;
+
+Uint32 getHex(int r, int g, int b) {
+    return SDL_MapRGB(SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888), r, g, b);
+}
 
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    if (IMG_Init(IMG_INIT_PNG) == 0) {
+        std::cerr << "SDL_image could not initialize! SDL_image error: " << IMG_GetError() << std::endl;
+        SDL_Quit();
         return -1;
     }
 
@@ -30,17 +44,31 @@ int main() {
         return -1;
     }
 
+    SDL_Surface* temp = IMG_Load("../Sprites/img.png");
+    SDL_Texture* playerImg = SDL_CreateTextureFromSurface(renderer, temp);
+    SDL_FreeSurface(temp);
+
     bool running = true;
     SDL_Event event;
 
-    int xPos = SCREEN_WIDTH / 2;
-    int yPos = SCREEN_HEIGHT / 2;
-    int velocity = 1;
+    double xPos = SCREEN_WIDTH / 2.0 - PLAYER_WIDTH / 2.0;
+    double yPos = SCREEN_HEIGHT / 2.0 - PLAYER_HEIGHT / 2.0;
+    // frames per second
+    int velocity = 500;
+
     bool w_held = false;
     bool a_held = false;
     bool s_held = false;
     bool d_held = false;
+
+    Uint64 lastTime = SDL_GetPerformanceCounter();
+    double dt = 0.0;
+
     while (running) {
+        Uint64 currentTime = SDL_GetPerformanceCounter();
+        dt = static_cast<double>(currentTime - lastTime) / SDL_GetPerformanceFrequency();
+        lastTime = currentTime;
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 running = false;
@@ -78,25 +106,35 @@ int main() {
                 }
             }
         }
-        if (w_held) yPos -= velocity;
-        if (a_held) xPos -= velocity;
-        if (s_held) yPos += velocity;
-        if (d_held) xPos += velocity;
+        if (w_held) yPos -= velocity * dt;
+        if (a_held) xPos -= velocity * dt;
+        if (s_held) yPos += velocity * dt;
+        if (d_held) xPos += velocity * dt;
 
-        if (xPos - CIRCLE_RADIUS < 0) xPos = CIRCLE_RADIUS;
-        if (xPos + CIRCLE_RADIUS > SCREEN_WIDTH) xPos = SCREEN_WIDTH - CIRCLE_RADIUS;
-        if (yPos - CIRCLE_RADIUS < 0) yPos = CIRCLE_RADIUS;
-        if (yPos + CIRCLE_RADIUS > SCREEN_HEIGHT) yPos = SCREEN_HEIGHT - CIRCLE_RADIUS;
+        // Make sure player does not go past edge of window
+        if (xPos < 0) xPos = 0;
+        if (xPos + PLAYER_WIDTH > SCREEN_WIDTH) xPos = SCREEN_WIDTH - PLAYER_WIDTH;
+        if (yPos < 0) yPos = 0;
+        if (yPos + PLAYER_WIDTH > SCREEN_HEIGHT) yPos = SCREEN_HEIGHT - PLAYER_WIDTH;
 
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Black background
         SDL_RenderClear(renderer);
 
-        filledCircleColor(renderer, xPos, yPos, CIRCLE_RADIUS, 0xFFFF0000);
+        // filledCircleColor(renderer, xPos, yPos, CIRCLE_RADIUS, getHex(255, 255, 255));
+        // Draw player onto screen
+        SDL_Rect playerRect = {static_cast<int>(std::round(xPos)), static_cast<int>(std::round(yPos)), PLAYER_WIDTH, PLAYER_HEIGHT};
+        SDL_RenderCopy(renderer, playerImg, nullptr, &playerRect);
 
         SDL_RenderPresent(renderer);
+
+        // Ensure fps stays at 60
+        if (dt < frameTime) {
+            SDL_Delay((frameTime - dt) * 1000);
+        }
     }
 
+    SDL_DestroyTexture(playerImg);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
